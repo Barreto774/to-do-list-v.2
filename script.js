12ae2dc65ba00b4f6b478b82a4f81fb9399915d5
Gabriel Barreto <gabrielbarreto206@outlook.com>
2022-10-24 19:13:44 -0300

    to-do-list-v.2

---
+++ script.js
@@ -0,0 +1,66 @@
+function adicionar() {
+    var conteudo = document.getElementById("texto").value;
+    if (conteudo != "") {
+        var novoElemento = document.createTextNode(conteudo);
+        var divNova = document.createElement("div");
+        divNova.className = "itens";
+        var botao = document.createElement("button");
+        var check = document.createElement("input");
+        check.setAttribute("type", "checkbox");
+
+        botao.innerHTML = "Remover";
+        botao.className = "button";
+        botao.style.color = "red";
+        botao.addEventListener("click", function() {
+            divNova.remove();
+        });
+        divNova.appendChild(check);
+        check.addEventListener("change", function() {
+            if (this.checked) {
+                divNova.style.textDecoration = "line-through";
+            } else {
+                divNova.style.textDecoration = "none";
+            }
+        });
+        divNova.appendChild(novoElemento);
+
+        divNova.appendChild(botao);
+        document.getElementById("tarefas").appendChild(divNova);
+        document.getElementById("texto").value = "";
+        document.getElementById("descricao").value = "";
+        document.getElementById("cad").style.display = "none";
+        document.getElementById("tarefas").style.display = "block";
+    }
+}
+
+function cadastrar() {
+    document.getElementById("cad").style.display = "block";
+    document.getElementById("tarefas").style.display = "none";
+}
+
+function exibir() {
+    document.getElementById("cad").style.display = "none";
+    document.getElementById("tarefas").style.display = "block";
+}
+
+document.addEventListener("keypress", function (e) {
+    if(e.key ==="Enter"){
+        const button = document.querySelector(".button");
+        
+        button.click();
+
+    }
+});
+
+function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin"){
+        location.href = "index.html";
+    }else{
+        alert('Usuario ou senha incorretos');
+    }
+
+}
