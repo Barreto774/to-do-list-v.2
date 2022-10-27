2b5444511cabb31c6ba5a4194c7504384b98a2df
Gabriel Barreto <gabrielbarreto206@outlook.com>
2022-10-27 11:44:09 -0300

    to do list v.4

--- script-index.js
+++ script-index.js
@@ -11,52 +11,91 @@ const form = document.getElementById('form');
 const nome = document.getElementById('nome');
 const login = document.getElementById('login');
 const email = document.getElementById('email');
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 const senha = document.getElementById('senha');
 const confsenha = document.getElementById('confsenha');
+const span = document.querySelectorAll('span');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+})
 
 function nomeValidar(){
-    if(nome.value.length < 10)
+    
+    if(nome.value == "")
     {
-        console.log("Nome menor que 10 caracteres");
+        nome.style.border=" 0px solid #fff";
+        span[0].style.display="none";
+    }
+    else if(nome.value.length < 10)
+    {
+        nome.style.border="3px solid #DB5A5A";
+        span[0].style.display="block";
     }
     else
     {
-        console.log("Nome Aceito");
+        nome.style.border="3px solid #4ECA64";
+        span[0].style.display="none";
     }
 }
 
 function loginValidar(){
-    if(login.value.length > 10)
+    if(login.value == "")
     {
-        console.log("Nome menor que 10 caracteres");
+        login.style.border=" 0px solid #fff";
+        span[1].style.display="none";
+    }
+    else if(login.value.length > 10)
+    {
+        login.style.border="3px solid #DB5A5A";
+        span[1].style.display="block";
     }
     else
     {
-        console.log("Nome Aceito");
+        login.style.border="3px solid #4ECA64";
+        span[1].style.display="none";
     }
 }
 
 function emailValidar(){
-    if(email.value.length > 10)
+    if(email.value == "")
     {
-        console.log("Tem @");
+        email.style.border=" 0px solid #fff";
+        span[2].style.display="none";
+    }
+    else if(!emailRegex.test(email.value))
+    {
+        email.style.border="3px solid #DB5A5A";
+        span[2].style.display="block";
     }
     else
     {
-        console.log("Não tem @");
+        email.style.border="3px solid #4ECA64";
+        span[2].style.display="none";
     }
 }
+
 function compareSenha(){
     if(senha.value == "" && confsenha.value == "" )
     {
-        console.log("Campo obrigatorio")
+        senha.style.border=" 0px solid #fff";
+        confsenha.style.border=" 0px solid #fff";
+        span[3].style.display="none";
+        span[4].style.display="none";
     }
     else if(senha.value == confsenha.value )
     {
-        console.log("Senhas Batem");
+        senha.style.border="3px solid #4ECA64";
+        confsenha.style.border="3px solid #4ECA64";
+        span[3].style.display="none";
+        span[4].style.display="none";
     }
     else
     {
-        console.log("Senhas não batem");
+        senha.style.border="3px solid #DB5A5A";
+        confsenha.style.border="3px solid #DB5A5A";
+        span[3].style.display="block";
+        span[4].style.display="block";
     }
 }
